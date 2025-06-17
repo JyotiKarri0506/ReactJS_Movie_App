7ab9e1c0068d89dd9db868369f984c9b9846a383
JyotiKarri0506 <jyotikarri0506@gmail.com>
2025-06-16 21:38:03 -0400

    updated index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import { useEffect, useState } from "react";
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0)
-  const [hasLiked, setHasLiked] = useState(false);
-  // useEffect runs twice when the component mounts
-  useEffect(() => {
-    console.log(`${title} has been Liked: ${hasLiked}`);
-  },[hasLiked]);
-
-  return (
-    <div className="card" onClick={()=> setCount(count +1)}>
-      <h2> {title} <br/> {count}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ?  "❤️" : "🤍"}
-      </button>
-    </div>
-  );
-};
+import React from 'react'
 
 const App = () => {
   return (
-    <div className="card-container">
-      <Card title="Movie0" />
-      <Card title="Movie1" />
-      <Card title="Movie2" />
+    <div>
+      <h1 className='text-3xl font-bold underline'>
+        Welcome Change 
+      </h1>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
